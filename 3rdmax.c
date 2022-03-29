bac5b606fe716b815a0fefd1b4e54cedb69045d7
TechnicalClub0322 <102578250+TechnicalClub0322@users.noreply.github.com>
2022-03-29 15:02:21 +0530

    Add files via upload

--- 3rdmax.c
+++ 3rdmax.c
@@ -3,11 +3,12 @@
 
 int main()
 {
-	long int arr[]={-12,-45,-67, -45 ,-34,-54,1,1,1,-500}, max, max2, max3;
+	long int arr[]={1,-2,-25,-21,-66,5,1,-35,-9,-13}, max, max2, max3,i,k;
 	max=max2=max3=-2147483648;
-	for(int k=1;k<=3;k++)
+	
+	for(k=1;k<=3;k++)
 	{
-		for(int i=0;i<10;i++)
+		for(i=0;i<10;i++)
 		{
 		if(arr[i]>=max)
 		{
